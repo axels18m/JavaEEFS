4c930e0ed68a4a21a9516c59b9bfac181fdb8c92
axels18m <axels18m@gmail.com>
2020-11-05 18:53:06 -0600

    Fixing workspace

--- Libreria/src/tests/Spring/messageHtml.java
+++ Libreria/src/tests/Spring/messageHtml.java
@@ -2,7 +2,7 @@ package tests.Spring;
 
 public class messageHtml  implements message
 {
-	@Override public void hola()
+	public void hola()
 	{
 		System.out.println("<html>Hola</html>");
 	}
--- Libreria/src/tests/Spring/principal.java
+++ Libreria/src/tests/Spring/principal.java
@@ -1,10 +1,26 @@
 package tests.Spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class principal 
 {
 	public static void main(String[] args)
 	{
+		principal p = new principal();
+		p.testAppContext();
+	}
+	
+	public void testMessage()
+	{
 		message msg = messageFactory.getMessage();
+		msg.hola();
+	}
+	
+	public void testAppContext()
+	{
+		ApplicationContext factory = new FileSystemXmlApplicationContext("applicationContext.xml");
+		message msg = (message) factory.getBean("messageHTML");
 		msg.hola();
 	}
 }
